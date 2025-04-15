eab0d547e45bf587108029d983326a0ee998ee33
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2025-04-15 20:44:31 -0300

    Componentes Mati

--- TP-REACT-1/Components/Botones/Vistas/Vistas.jsx
+++
@@ -1,7 +0,0 @@
-import style from './Vistas.module.css' 
-
-const BotonVistas = () => {
-    return <button className = {style.estiloBotonVistas}>Vistas / No vistas</button> 
-}; 
-
-export default BotonVistas
