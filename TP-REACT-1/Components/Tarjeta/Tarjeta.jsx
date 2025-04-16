aaa8ea564916db81e5fdcbfba2a0aceef60737d9
CamilaFuentess <camila.fuentes@est.fi.uncoma.edu.ar>
2025-04-16 20:25:16 -0300

    Modificacion de componentes

---
+++ TP-REACT-1/Components/Tarjeta/Tarjeta.jsx
@@ -0,0 +1,8 @@
+// Componente separado del contenedorpelis
+// RECIBE LA INFORMACION DE LA PELICULA A MOSTRAR A TRAVES DE UN ARREGLO 
+// return dentro del map 
+// contenedorPelis, recibe el filtro del boton y el arreglo de TODAS LAS PELIS 
+
+//ACA IRIA EL POP, MOSTRANDO EL TARJETA RESUMEN. 
+//CUANDO SE AGREGA UNA PELI, SE VA AL LOCALSTORAGE. 
+// 
--- TP-REACT-1/Pages/Home/Home.jsx
+++ TP-REACT-1/Pages/Home/Home.jsx
@@ -5,15 +5,19 @@ import InputBusqueda from '../../Components/InputBusqueda/InputBusqueda';
 
 const Home = () => {
   const [filtro, setFiltro] = useState({ tipo: '', valor: '' });
-
+  //FUNCION QUE BUSCA EN EL LOCALSTORAGE 
+  //aca en home iria la funcion que crea/carga el arreglo desde el local
   const manejarFiltro = (tipo, valor) => {
     setFiltro({ tipo, valor });
   };
 
   return (
     <div >
-      <BarraDeInicio manejarFiltro={manejarFiltro} />
-      <InputBusqueda manejarFiltro={manejarFiltro}/>
+      <header>
+        <BarraDeInicio manejarFiltro={manejarFiltro} />
+        <InputBusqueda manejarFiltro={manejarFiltro}/>
+      </header>
+      
       <ContenedorPelis filtro={filtro} />
     </div>
   );
