f3fd405fae026bc4ad6b59b563407b23219cc61a
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2025-04-30 15:37:20 -0300

    Actualizacion

--- TP-REACT-2/TP-REACT-2/Components/Titulo/Titulo.jsx
+++ TP-REACT-2/TP-REACT-2/Components/Titulo/Titulo.jsx
@@ -4,7 +4,7 @@ const Titulo = ({texto}) => {
 
     return (
         <>
-            <div class="max-w-sm  items-center p-2 bg-sky-500/40 self-center ">
+            <div class=" items-center p-2 bg-sky-500/40 self-center box-content rounded-2xl">
                 <h1 class="mx-auto">{texto}</h1>
             </div>
             
