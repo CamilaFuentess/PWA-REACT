16a45a71b9093f4e9f6f99e36bacddf1c2b0dea0
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2025-04-30 10:29:31 -0300

    Tailwind

--- TP-REACT-2/TP-REACT-2/Pages/Info/Info.jsx
+++ TP-REACT-2/TP-REACT-2/Pages/Info/Info.jsx
@@ -3,18 +3,30 @@ import React from "react";
 import Titulo from '../../Components/Titulo/Titulo';
 import ArticuloImagen from "../../Components/ArticuloImagen/ArticuloImagen";
 import ArticuloPrecio from "../../Components/ArticuloPrecio/ArticuloPrecio";
+import TarjetaInfo from "../../Components/TarjetaInfo/TarjetaInfo";
 
 const Info = ({articulo}) => {
 
     return (
         
             <div class="block items-center bg-amber-400 ">
+                <div class="box-border border-4 p-1 rounded-2xl">
                     <Titulo texto={articulo.nombre}></Titulo>
-                <div class="grid grid-cols-2">
-                    <ArticuloImagen articulo={articulo}></ArticuloImagen>
-                    <ArticuloPrecio articulo={articulo}></ArticuloPrecio>
-
                 </div>
+                <div class="grid grid-cols-2 m-2 p-2">
+                    <div class="grid grid-rows-2">
+                        <div class="box-content size-59% border-4 p-4 m-1 rounded-2xl">
+                            <ArticuloImagen articulo={articulo}></ArticuloImagen>
+                        </div>
+                        <div class="box-content size-40% border-4 p-4 m-4 rounded-2xl" >
+                            <ArticuloPrecio articulo={articulo}></ArticuloPrecio>
+                        </div>
+                    </div>
+                    <div class="box-border size-39% border-4 p-4 rounded-2xl m-4">
+                    <TarjetaInfo articulo={articulo}></TarjetaInfo>
+                    </div>
+                </div>
+
 
             </div>
         
