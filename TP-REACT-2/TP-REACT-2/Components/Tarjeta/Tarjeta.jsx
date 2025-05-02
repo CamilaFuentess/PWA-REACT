f25f6d9f4bc9a21a32a5b5b2a90103bfc0aeca81
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2025-05-02 16:27:55 -0300

    actualizado info

--- TP-REACT-2/TP-REACT-2/Components/Tarjeta/Tarjeta.jsx
+++ TP-REACT-2/TP-REACT-2/Components/Tarjeta/Tarjeta.jsx
@@ -34,7 +34,7 @@ const Tarjeta = ({ producto }) => {
 
     return (
       <div className="bg-[#3c3a3a] border-10 border-[#b7c2ce] rounded-3xl cursor-pointer transition p-4 min-h-[250px] max-w-sm mx-auto [box-shadow:0_0_6px_#b7c2ce,0_0_12px_#b7c2ce]
-             hover:[box-shadow:0_0_10px_#b7c2ce,0_0_20px_#b7c2ce]" onClick={clickTarjeta}>
+             hover:[box-shadow:0_0_10px_#b7c2ce,0_0_20px_#b7c2ce]" onClick={() => clickTarjeta(producto.id)}>
         <div className="relative">
           <img src={producto.coverImage} className="w-full h-44 rounded-3xl object-cover"/>
           <div className="absolute top-2 right-4 z-10">
@@ -44,7 +44,7 @@ const Tarjeta = ({ producto }) => {
         <div className="text-center mt-2">
           <h3 className="text-base font-semibold text-[#ebeef3]">{producto.name}</h3>
           <div className='mt-3'> 
-            <button className="bg-[#f9fafc] text-[#3c3a3a] font-bold  px-6 py-3 rounded-full transition transform hover:scale-105 hover:shadow-lg" onClick={clickTarjeta}> Ver detalle</button>
+            <button className="bg-[#f9fafc] text-[#3c3a3a] font-bold  px-6 py-3 rounded-full transition transform hover:scale-105 hover:shadow-lg" onClick={() => clickTarjeta(producto.id)}> Ver detalle</button>
           </div>
         </div>
       </div>
--- TP-REACT-2/TP-REACT-2/Pages/Info/Info.jsx
+++ TP-REACT-2/TP-REACT-2/Pages/Info/Info.jsx
@@ -1,41 +1,73 @@
 import React, { useState, useEffect } from "react";
 import Titulo from '../../Components/Titulo/Titulo';
+import { useParams } from 'react-router-dom';
+
+import { useLocation } from 'react-router-dom';
 
 
 
-
-const Info = ({articulo}) => {
-   // const { id } = useParams();
+const Info = () => {
+  //  const { id } = useParams();
+    const location = useLocation();
+    const id = location.state?.id;
+    console.log("id: ", id)
 
     const [computadora, setComputadora] = useState();
-    const [searchValue, setSearchValue] = useState("1");
+    const [detalle, setDetalle] = useState();
+    const [searchValue, setSearchValue] = useState(id);
 
     //const details = ({valor, art}) => {
 
         const getComputadoras = async () => {
             try {
                 const computadoraResultado = await fetch(
-                    `https://6810f18027f2fdac24136e06.mockapi.io/api/v1/detalleComputadoras/${searchValue}`
+                    `https://6810f18027f2fdac24136e06.mockapi.io/api/v1/computadoras/${searchValue}`
                 );
                 const compu = await computadoraResultado.json(); 
-
                 setComputadora(compu);
             } catch (error){
                 console.log('0020', error);
             }
         }
-
         useEffect(() => {
             getComputadoras();
         }, []);
 
-        
+        const getDetalles = async () => {
+            try {
+                const detallesResultado = await fetch(
+                    `https://6810f18027f2fdac24136e06.mockapi.io/api/v1/detalleComputadoras/${searchValue}`
+                );
+                const deta = await detallesResultado.json(); 
+                setDetalle(deta);
+            } catch (error){
+                console.log('0020', error);
+            }
+        }
+     
+        useEffect(() => {
+            getDetalles();
+        }, []);    
     //}
     
-    //details("1")
-    console.log(computadora);
 
     if (computadora === undefined) {
+        const d = 
+        {
+            id: "1",
+            idComputadora: "1",
+            marca: "AMD",
+            modelo: "Ryzen 5 5600X",
+            fechaLanzamiento: "2020-11-05",
+            especificaciones: [
+              "Socket AM4",
+              "6 núcleos / 12 hilos",
+              "Frecuencia base 3.7GHz",
+              "Frecuencia turbo 4.6GHz",
+              "TDP 65W",
+              "Tecnología de 7nm"
+            ]
+          }
         return <h1>Loading....</h1>;
     }
 
@@ -45,26 +77,24 @@ const Info = ({articulo}) => {
             <div className="block items-center bg-blue-600 box-border p-4">
 
                 <div className="box-border border-4 p-1 m-1 rounded-2xl">
-                    <Titulo texto={articulo.name}></Titulo>
+                    <Titulo texto={computadora.name}></Titulo>
                 </div>
                 <div className="grid grid-cols-2 m-1">
                     <div className="grid grid-rows-2">
                         <div className="box-content size-70% border-4 p-4 m-1 rounded-2xl">
-                            <img src={articulo.coverImage} alt="Producto" className="box-border rounded-2xl" />
+                            <img src={computadora.imagen} alt="Producto" className="box-border rounded-2xl" />
                         </div>
                         <div className="box-content size-30% border-4 p-4 m-4 rounded-2xl" >
-                            <h1 className="text-8xl text-white ">${articulo.precio}</h1>
+                            <h1 className="text-8xl text-white ">${computadora.precio}</h1>
                         </div>
                     </div>
                     <div className="box-border size-30% border-4 rounded-2xl m-1">
-                        <h1 className="text-2xl text-white">{articulo.name}</h1>
-                        <h1 className="text-2xl text-white">{computadora.marca}</h1>
-                        <h1 className="text-2xl text-white">{computadora.modelo}</h1>
-                        <h1 className="text-2xl text-white">{computadora.especificaciones}</h1>
+                        <h1 className="text-2xl text-white">{computadora.name}</h1>
+                        <h1 className="text-2xl text-white">{detalle.marca}</h1>
+                        <h1 className="text-2xl text-white">{detalle.modelo}</h1>
+                        <h1 className="text-2xl text-white">{detalle.especificaciones}</h1>
                     </div>
-
                 </div>
-
             </div>
         
     );
@@ -72,7 +102,6 @@ const Info = ({articulo}) => {
 };
 
 export default Info;
-
 
 
 
